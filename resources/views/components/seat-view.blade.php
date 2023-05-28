f8f359b83d51309f69ce1ebb33dca111cbf6d3fa
nvmttq <nvmttq@gmail.com>
2023-05-28 19:45:45 +0700

    fix responsive mobile max 400

--- resources/views/components/seat-view.blade.php
+++ resources/views/components/seat-view.blade.php
@@ -3,6 +3,6 @@
 
 
 
-<a href = '{{$link}}'{{ $attributes->merge(['class' => 'seat-button text-center font-bold hover:bg-blue-700 text-white py-1 px-[25px] rounded-lg w-36 bg-[var(--primary-color)] cursor-pointer']) }}>
+<a href = '{{$link}}'{{ $attributes->merge(['class' => 'seat-button text-center font-bold hover:bg-blue-700 text-white py-2 px-[25px] rounded-lg w-36 bg-[var(--primary-color)] cursor-pointer']) }}>
     {{ $slot }}
 </a>
